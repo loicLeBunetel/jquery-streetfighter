64a02c44e2e4b3e330ca1e69476c06f5c826578e
Loic LE BUNETEL <loic.lebunetel@yahoo.fr>
2014-09-25 16:02:56 +0200

    assignment 8 complete

--- js/app.js
+++ js/app.js
@@ -1,18 +1,22 @@
 
 $(document).ready(function(){
+
+	$('#street-sound')[0].volume = 0.3;
+	$('#street-sound')[0].play();
+
 	$(".ryu").mouseenter(function(){
-		$(".ryu-still").hide();
+		$(".ryu-action").hide();
 		$(".ryu-ready").show();
 	})
 
 	.mouseleave(function(){
+		$(".ryu-action").hide();
 		$(".ryu-still").show();
-		$(".ryu-ready").hide();
 	})
 
 	.mousedown(function(){
 		playHadouken();
-		$(".ryu-ready").hide();
+		$(".ryu-action").hide();
 		$(".ryu-throwing").show();
 		$(".hadouken").finish().show()
 		.animate(
@@ -27,14 +31,36 @@ $(document).ready(function(){
 	})
 
 	.mouseup(function(){
-		console.log('mouseup');
-		$(".ryu-throwing").hide();
-		$(".ryu-ready").show();
+		$(".ryu-action").hide();
+		$(".ryu-still").show();
 	});
+
+	$(document).keydown(function() {
+		if(event.which == 88){
+			playCool();
+			$(".ryu-action").hide();
+			$(".ryu-cool").show();
+		}
+	})
+
+	.keyup(function(){
+		$('#cool-sound')[0].pause();
+		$('#cool-sound')[0].load();
+		$(".ryu-action").hide();
+		$(".ryu-still").show();
+	});
+
+
 });
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
+}
+
+function playCool () {
+  $('#cool-sound')[0].volume = 0.5;
+  //$('#cool-sound')[0].load();
+  $('#cool-sound')[0].play();
 }
